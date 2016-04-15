1ce6b7ae0de260d65dd85125cbee3204165a4a72
root <root@debian-test.local>
2016-04-15 10:47:36 +0200

    bug

--- CONFIGURATION/SECURITY/ROOTKIT/install_rootkit.sh
+++ CONFIGURATION/SECURITY/ROOTKIT/install_rootkit.sh
@@ -5,5 +5,5 @@
 #	Copy file configuration rkhunter
 #
 
-rsync -av --progress chkrootkit.cron /etc/cron.d/chkrootkit
+rsync -av --progress $PATH_CONFIGURATION/SECURITY/ROOTKIT/chkrootkit.cron /etc/cron.d/chkrootkit
 echo "0 6 * * * root (/usr/sbin/chkrootkit 2>&1 | mutt -s '[$HOSTNAME] Résultats de chkrootkit' $MAIL)" >> /etc/cron.d/chkrootkit
