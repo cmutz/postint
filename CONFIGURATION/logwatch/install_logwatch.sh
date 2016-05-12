959e96327530720f689ffccb186c712e362e015b
root <root@whoople-odoo-9-ocb.novalocal>
2016-05-12 14:01:30 +0000

    bug sed

--- CONFIGURATION/logwatch/install_logwatch.sh
+++ CONFIGURATION/logwatch/install_logwatch.sh
@@ -4,4 +4,4 @@
 #
 #	Copy file configuration logwatch
 #
-sed -i 's/\/usr\/sbin\/logwatch --output --detail high/\/usr\/sbin\/logwatch --outputmail --mailto '$MAIL'  --detail high/g' /etc/cron.daily/00logwatch
+sed -i 's/\/usr\/sbin\/logwatch --output mail/\/usr\/sbin\/logwatch --outputmail --mailto '$MAIL'  --detail high/g' /etc/cron.daily/00logwatch
