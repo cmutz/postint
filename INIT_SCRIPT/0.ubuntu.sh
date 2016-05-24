cc55aed04727f966eac7fa6c77a247b9d94d1c58
Mutz Clement <c.mutz@whoople.fr>
2016-05-24 10:50:14 +0200

    add dist-upgrade for ubuntu system

---
+++ INIT_SCRIPT/0.ubuntu.sh
@@ -0,0 +1,24 @@
+###############################################################################
+#		Mise à niveau du system
+###############################################################################
+
+### import file functions ###
+. $PATH_LIBRARY/functions.sh
+### END import file functions ###
+
+set -u  # u pour envoyer sur l'entrée standard les variables non définies
+
+
+pkg_manager="apt-get"
+
+if [[ $INSTALL_AUTO = yes ]]; then
+	$pkg_manager -yf dist-upgrade
+else
+	if ask_yn_question "\tMettre à jour les paquets de cette machine ?"; then
+    	println info "Faire \" $pkg_manager upgrade ... \" "
+		$pkg_manager -yf dist-upgrade
+	fi
+fi
+
+################################################################################
+
